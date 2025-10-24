f4b2349f834865898907fce7e2f2236a5b4501b3
Ashvin Santhosh <vrindtime@gmail.com>
2025-10-25 00:36:42 +0530

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3368566086"
+    serviceWorkerVersion: "2584031318"
   }
 });
