533fbf6f8e8ab31f412fa1078c6d2f14f20867df
Ashvin Santhosh <vrindtime@gmail.com>
2025-10-30 01:10:32 +0530

    added ability to view chat betwen org and client

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "540651045"
+    serviceWorkerVersion: "1968726619"
   }
 });
